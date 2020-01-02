16b6a7ae5dd5b9d164c2110e72afb2b8f60d6570
wd1204@uw.edu <wd1204@uw.edu>
2020-01-02 12:38:32 -0800

    course schedule |||

---
+++ courseScedule3.py
@@ -0,0 +1,23 @@
+import heapq
+
+class Solution(object):
+    def scheduleCourse(self, courses):
+        """
+        :type courses: List[List[int]]
+        :rtype: int
+        """
+        # sort by earlest duedate
+        courses.sort(key=lambda k: k[1])
+
+        que = []
+        cur = 0
+
+        # use min heap to remove biggest duration
+        for duration, due in courses:
+            heapq.heappush(que, -duration)
+            cur += duration
+            if cur > due:
+                removed = heapq.heappop(que)
+                cur += removed
+
+        return len(que)
