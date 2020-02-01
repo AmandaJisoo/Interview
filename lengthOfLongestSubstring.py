60ccc876d9e4cb04fba8e8810cf3f2d8f649f50d
AMANDA JISOO PARK <wd1204@uw.edu>
2020-01-31 16:46:17 -0800

    comments added

--- lengthOfLongestSubstring.py
+++ lengthOfLongestSubstring.py
@@ -13,14 +13,19 @@ class Solution(object):
         character = set()
         longest = 0
         while (bPointer < len(s)):
+            #if the back pointer is a new character encoutnering
+            #add to set and proceed the back pointer and update the distance
             if s[bPointer] not in character:
                 character.add(s[bPointer])
                 bPointer += 1
                 longest = max((len(character)), longest)
+            #if duplicate, remove the start point of the search
+            #update the pointer so the begining of the word will be 
+            # next word
             else:
                 character.remove(s[aPointer])
                 aPointer += 1
         return longest
-
+s
 
 
